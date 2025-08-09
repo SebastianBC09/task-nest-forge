a771d66ffd5588baa8028865f281b6105f4bd4a3
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-08-09 12:32:52 -0500

    feat(application): add TasksModule configuration

---
+++ src/application/tasks.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { CreateTaskService } from './services/create-task.service';
+import { UpdateTaskService } from './services/update-task.service';
+import { ChangeTaskStatusService } from './services/change-task-status.service';
+import { ListTasksService } from './services/list-tasks.service';
+import { DeleteTaskService } from './services/delete-task.service';
+import { TasksController } from '../infrastructure/http/controllers/tasks.controller';
+import { PersistenceModule } from '../infrastructure/persistence/persistance.module';
+import { AuthModule } from '../infrastructure/auth/auth.module';
+import { UsersModule } from './users.module';
+
+@Module({
+  imports: [PersistenceModule, AuthModule, UsersModule],
+  controllers: [TasksController],
+  providers: [
+    CreateTaskService,
+    UpdateTaskService,
+    ListTasksService,
+    ChangeTaskStatusService,
+    DeleteTaskService,
+  ],
+})
+export class TasksModule {}

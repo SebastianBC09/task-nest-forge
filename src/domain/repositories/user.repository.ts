b5b7715230dac77c7d09707c4012a9d692c353b7
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-08-09 12:25:20 -0500

    refactor(domain): enhance UserRepository interface with CRUD operations

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -1,7 +1,7 @@
 import { User } from '../entities/user.entity';
 
 export interface UserRepository {
-  create(name?: unknown): Promise<User>;
+  create(name?: string | null): Promise<User>;
   findById(id: string): Promise<User | null>;
-  findOrCreateById(id: string, name?: unknown): Promise<User | null>;
+  findOrCreateById(id: string, name?: string | null): Promise<User>;
 }

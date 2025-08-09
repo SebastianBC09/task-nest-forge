37daa447b5bbdde788257d035b3f2a4f843efe99
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-08-09 12:50:36 -0500

    feat: configure global validation pipe and Prisma shutdown hooks

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { PrismaService } from './infrastructure/persistence/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const prisma = app.get(PrismaService);
+  prisma.enableShutdownHooks(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

fb54bc380ad65edf0efaa3dec42ecc7ebf7e6387
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-08-09 12:50:28 -0500

    feat: update AppModule with new module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './infrastructure/persistence/prisma.module';
+import { PrismaService } from './infrastructure/persistence/prisma.service';
+import { AuthModule } from './infrastructure/auth/auth.module';
+import { PersistenceModule } from './infrastructure/persistence/persistance.module';
+import { TasksModule } from './application/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, PersistenceModule, AuthModule, TasksModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}

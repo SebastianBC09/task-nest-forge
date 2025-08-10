a0400c8da658bef16ae25226a2d710a730322937
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-08-09 21:43:53 -0500

    fix: update import path for PersistenceModule

--- src/application/tasks.module.ts
+++ src/application/tasks.module.ts
@@ -5,7 +5,7 @@ import { ChangeTaskStatusService } from './services/change-task-status.service';
 import { ListTasksService } from './services/list-tasks.service';
 import { DeleteTaskService } from './services/delete-task.service';
 import { TasksController } from '../infrastructure/http/controllers/tasks.controller';
-import { PersistenceModule } from '../infrastructure/persistence/persistance.module';
+import { PersistenceModule } from '../infrastructure/persistence/persistence.module';
 import { AuthModule } from '../infrastructure/auth/auth.module';
 import { UsersModule } from './users.module';
 

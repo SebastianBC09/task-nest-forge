ac800cc1f5cd92eb2a8cc12a15e7edfe8b46dcfe
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-08-09 21:43:51 -0500

    fix: update import path for PersistenceModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { PrismaModule } from './infrastructure/persistence/prisma.module';
 import { PrismaService } from './infrastructure/persistence/prisma.service';
 import { AuthModule } from './infrastructure/auth/auth.module';
-import { PersistenceModule } from './infrastructure/persistence/persistance.module';
+import { PersistenceModule } from './infrastructure/persistence/persistence.module';
 import { TasksModule } from './application/tasks.module';
 
 @Module({
